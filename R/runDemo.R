3b639ab46fd470d95b109ef570b75f0c57174e0b
Vhain38 <massimo@finini.ch>
2021-11-26 16:44:52 +0100

    Package update
    Correction of bugs in an attempt to make the package downloadable

--- R/runDemo.R
+++ R/runDemo.R
@@ -1,4 +1,10 @@
-#' @export
+#'@title Application Demo
+#'@authors Marie Bellier, Massimo Finini, Meri Likoska, Vania Rodrigues Telo Ramos, Xavier Renger
+#'@param ... Nothing
+#'@return launch the shiny application
+#'@example runDemo()
+#'@export
+
 runDemo <- function() {
   appDir <- system.file("shiny-examples", "area", package = "pkghw4g5")
   if (appDir == "") {
@@ -12,5 +18,3 @@ runDemo <- function() {
 
 }
 
-
-runDemo()
